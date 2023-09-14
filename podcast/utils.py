9bbbe8fa817dbe1f2aa0d1e65c498eabd46abad7
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-14 15:42:12 -0700

    feat: parser for podcast and episode added

---
+++ podcast/utils.py
@@ -0,0 +1,33 @@
+import xml.etree.ElementTree as ET
+from .models import Podcast, Episode
+
+class PodcastParser:
+    def __init__(self, xml_file):
+        self.tree = ET.parse(xml_file)
+        self.root = self.tree.getroot()
+        self.namespaces = {
+            'itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd',
+        }
+
+    def get_text(self, node, path, default=""):
+        element = node.find(path, namespaces=self.namespaces)
+        return element.text if element is not None else default
+
+    def parse(self):
+        channel = self.root.find('channel')
+        if channel:
+            podcast = Podcast.objects.create(
+                title=self.get_text(channel, 'title'),
+                generator=self.get_text(channel, 'generator'),
+                description=self.get_text(channel, 'description'),
+                copyright=self.get_text(channel, 'copyright'),
+                language=self.get_text(channel, 'language')
+            )
+
+            for item in channel.findall('item'):
+                Episode.objects.create(
+                    podcast=podcast,
+                    title=self.get_text(item, 'title'),
+                    description=self.get_text(item, 'description'),
+                    pubDate=self.get_text(item, 'pubDate')
+                )
