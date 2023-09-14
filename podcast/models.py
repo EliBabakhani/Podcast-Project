c02fb70b919f884a8b8ea3e9c4476790232911f1
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-14 15:43:55 -0700

    feat: podcast model added

--- podcast/models.py
+++ podcast/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Podcast(models.Model):
+    title = models.CharField(max_length=200)
+    generator = models.CharField(max_length=200)
+    description = models.TextField()
+    copyright = models.CharField(max_length=200)
+    language = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class Episode(models.Model):
+    podcast = models.ForeignKey(Podcast, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    pubDate = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
