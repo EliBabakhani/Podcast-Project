73c31e7ac694119ab7cbc36615645469094c0980
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-18 19:06:49 -0700

    feat: method for hash passwords

--- API/serializer_user.py
+++ API/serializer_user.py
@@ -10,13 +10,13 @@ class UserSeriallizer(serializers.ModelSerializer):
             'password':{'write_only':True}
         }
 
-        def create(self, validated_data):
-            password=validated_data.pop('password', None)
-            instance=self.Meta.model(**validated_data)
-            if password is not None:
-                instance.set_password(password)
-            instance.save()
-            return instance
+    def create(self, validated_data):
+        password=validated_data.pop('password', None)
+        instance=self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
 
 
