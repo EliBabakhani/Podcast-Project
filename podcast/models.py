7c342b68f997721726fb59ebe444432ce2593e35
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-19 20:00:46 -0700

    feat: podcast model created

--- podcast/models.py
+++ podcast/models.py
@@ -2,10 +2,20 @@ from django.db import models
 
 class Podcast(models.Model):
     title = models.CharField(max_length=200)
-    generator = models.CharField(max_length=200)
+    generator = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField()
-    copyright = models.CharField(max_length=200)
-    language = models.CharField(max_length=50)
+    copyright = models.CharField(max_length=200, null=True, blank=True)
+    language = models.CharField(max_length=50, null=True, blank=True)
+    link=models.CharField(max_length=100, null=True, blank=True)
+    pubDate=models.CharField(max_length=50, null=True, blank=True)#edit to datefield
+    itunes_summary = models.CharField(max_length=200, null=True, blank=True)
+    itunes_type = models.CharField(max_length=50, null=True, blank=True)
+    itunes_explicit = models.CharField(max_length=50, null=True, blank=True)
+    itunes_author = models.ForeignKey('Author', on_delete=models.CASCADE, null=True, blank=True)
+    itunes_category = models.CharField(max_length=100, null=True, blank=True)
+    managingEditor = models.CharField(max_length=100, null=True, blank=True)
+    owner=models.ForeignKey('Owner', on_delete=models.CASCADE, null=True, blank=True)
+    image=models.ForeignKey('Image', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.title
