cda0a9e160047676719a9e2302529b92a8dcc625
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-19 20:01:55 -0700

    feat: author model created

--- podcast/models.py
+++ podcast/models.py
@@ -45,4 +45,10 @@ class Image(models.Model):
     title=models.CharField(max_length=100)
 
     def __str__(self) -> str:
-        return self.title
+    
+class Author(models.Model):
+    name=models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
