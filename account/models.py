13613785dd44f18e72adbea79864cb15810f2884
Elnaz Babakhani <elnaz.babakhani71@gmail.com>
2023-09-26 17:52:22 -0700

    chore: edit user model- login with username

--- account/models.py
+++ account/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     name=models.CharField(max_length=100)
-    email=models.EmailField(max_length=100, unique=True)
+    email=models.EmailField(max_length=100)
     password=models.CharField(max_length=100)
-    username=models.CharField(max_length=50)
+    username=models.CharField(max_length=50, unique=True)
 
-    USERNAME_FIELD='email'
+    USERNAME_FIELD='username'
     REQUIRED_FIELDS=[]
